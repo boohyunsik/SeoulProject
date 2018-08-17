5d6875558a0ca8166718097eb5598e968b30bf96
YongDavidlLee <kstuve12@naver.com>
2018-08-17 21:03:26 +0900

    info_frgament 1. image insertion 2. title insertion 3. readmoreText 구현

--- app/src/main/java/com/caucse/seoulproject/fragment/InfoFragment.kt
+++ app/src/main/java/com/caucse/seoulproject/fragment/InfoFragment.kt
@@ -1,15 +1,28 @@
 package com.caucse.seoulproject.fragment
 
 import android.content.Context
+import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.ImageView
+import android.widget.TextView
 
 import com.caucse.seoulproject.R
 import com.caucse.seoulproject.data.CultureRow
+import com.nhn.android.maps.nmapdata.t
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_info.*
+import kotlinx.android.synthetic.main.fragment_info.view.*
+import kotlinx.android.synthetic.main.nav_header_list.*
+
 class InfoFragment : Fragment() {
 
     private var listener: OnFragmentInteractionListener? = null
@@ -22,12 +35,34 @@ class InfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        var view: View= inflater.inflate(R.layout.fragment_info, container, false)
+        var readView:TextView = view.findViewById(R.id.readMoreTextView)
+        var titleView:TextView = view.findViewById(R.id.titleView)
+        var titleContent =  "타이틀입니다"
+        var infoContent = """testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
+testingtestingtestingtestingtestingtestingtestingtesting
+testingtestingtestingtestingtestingtestingtestingtestingtesting
+testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
+testingtestingtestingtestingtestingtestingtestingtestingtesting
+testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
+testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting이부분 안나오나"""
+      //  Log.d("error",R.drawable.ic_test_info.toString())
+        titleView.setText(titleContent)
+        readView.setText(infoContent)
+        return view
     }
+    // TODO: Insert Naver Map
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var imageView: ImageView = view.findViewById(R.id.infoImageView)
+        imageView.setImageResource(R.drawable.ic_test_info)
 
+    }
     // TODO: Rename method, update argument and hook method into UI event
+
     fun onButtonPressed(uri: Uri) {
         listener?.onFragmentInteraction(uri)
+
     }
 
     override fun onAttach(context: Context) {
