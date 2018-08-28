7b2c3855edefc5f6dd3e58368bd25a74378fff72
YongDavidlLee <kstuve12@naver.com>
2018-08-28 16:01:57 +0900

    네이버 지도 추가

--- app/src/main/java/com/caucse/seoulproject/fragment/InfoFragment.kt
+++ app/src/main/java/com/caucse/seoulproject/fragment/InfoFragment.kt
@@ -22,15 +22,24 @@ import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_info.*
 import kotlinx.android.synthetic.main.fragment_info.view.*
 import kotlinx.android.synthetic.main.nav_header_list.*
+import com.nhn.android.maps.NMapActivity
+import com.nhn.android.maps.NMapContext
+import com.nhn.android.maps.NMapView
+import java.sql.ClientInfoStatus
+
 
 class InfoFragment : Fragment() {
 
     private var listener: OnFragmentInteractionListener? = null
+    private lateinit var mMapView:NMapView
+    private lateinit var mMapContext:NMapContext
 
+    private  var CLIENT_ID = "ILMvRUaQNzCAYsTvxb59"
     private lateinit var cultureData : CultureRow
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -38,24 +47,25 @@ class InfoFragment : Fragment() {
         var view: View= inflater.inflate(R.layout.fragment_info, container, false)
         var readView:TextView = view.findViewById(R.id.readMoreTextView)
         var titleView:TextView = view.findViewById(R.id.titleView)
+        var mapView:NMapView = view.findViewById(R.id.mapView)
         var titleContent =  "타이틀입니다"
-        var infoContent = """testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
-testingtestingtestingtestingtestingtestingtestingtesting
-testingtestingtestingtestingtestingtestingtestingtestingtesting
-testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
-testingtestingtestingtestingtestingtestingtestingtestingtesting
-testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
-testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting이부분 안나오나"""
-      //  Log.d("error",R.drawable.ic_test_info.toString())
+        var infoContent = """test"""
+        var imageView: ImageView = view.findViewById(R.id.infoImageView)
+        var mMapContext:NMapContext = NMapContext(super.getActivity())
+        mMapContext.onCreate()
+
+
+
         titleView.setText(titleContent)
         readView.setText(infoContent)
-
-        var imageView: ImageView = view.findViewById(R.id.infoImageView)
         imageView.setImageResource(R.drawable.ic_test_info)
+        mapView.setClientId(CLIENT_ID)
+        mMapContext.setupMapView(mapView)
+
 
         return view
     }
-    // TODO: Insert Naver Map
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
