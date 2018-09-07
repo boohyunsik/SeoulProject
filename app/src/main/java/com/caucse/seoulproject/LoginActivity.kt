e614a14a130c119ee3c58e821ebe02dfc8b0d414
Juryun <id01096883446@gmail.com>
2018-09-07 23:15:30 +0900

    로그인설정 v.0.2

--- app/src/main/java/com/caucse/seoulproject/LoginActivity.kt
+++ app/src/main/java/com/caucse/seoulproject/LoginActivity.kt
@@ -1,16 +1,11 @@
 package com.caucse.seoulproject
 
-import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.caucse.seoulproject.R
 import com.nhn.android.naverlogin.OAuthLogin
-import com.nhn.android.naverlogin.OAuthLoginDefine
 import com.nhn.android.naverlogin.OAuthLoginHandler
 import kotlinx.android.synthetic.main.activity_login.*
-import android.widget.Toast
-import android.databinding.adapters.TextViewBindingAdapter.setText
 import android.util.Log
 
 
@@ -30,10 +25,17 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        var initialToken = mOAuthLoginInstance.getAccessToken(this@LoginActivity)
+        if(initialToken != null) {
+            startCulin()
+        }
+        else{
+            Log.d("초기 로그인상태",mOAuthLoginInstance.getState(this@LoginActivity).toString())
+        }
         var loginButton = this.naverLogin
-        loginButton.setOAuthLoginHandler(mOAuthLoginHandler)
-        mOAuthLoginInstance.getAccessToken()
+            loginButton.setOAuthLoginHandler(mOAuthLoginHandler)
     }
+
     fun startCulin(){
         var intent = Intent(this, MainActivity::class.java)
         // TODO : 이런식으로 intent.putExtra(key, value) 식으로 로그인 정보 전달
@@ -51,10 +53,14 @@ class LoginActivity : AppCompatActivity() {
     private val mOAuthLoginHandler = object : OAuthLoginHandler() {
         override fun run(success: Boolean) {
             if (success) {
+                val accessToken = mOAuthLoginInstance.getAccessToken(this@LoginActivity)
                 Log.d("로그인", "로그인성공")
+                Log.d("성공토큰타입", accessToken)
                 startCulin()
             } else {
+                val accessToken = mOAuthLoginInstance.getAccessToken(this@LoginActivity)
                 Log.d("로그인", "로그인실패")
+                Log.d("실패토큰", accessToken)
             }
         }
     }
