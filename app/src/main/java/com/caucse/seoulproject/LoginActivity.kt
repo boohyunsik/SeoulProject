ddd321172984987cd7e20db5c774e9b0a7dbf7b4
Juryun <id01096883446@gmail.com>
2018-09-07 18:52:14 +0900

    로그인 설정 v.0.1

--- app/src/main/java/com/caucse/seoulproject/LoginActivity.kt
+++ app/src/main/java/com/caucse/seoulproject/LoginActivity.kt
@@ -1,28 +1,63 @@
 package com.caucse.seoulproject
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.caucse.seoulproject.R
+import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLoginDefine
+import com.nhn.android.naverlogin.OAuthLoginHandler
 import kotlinx.android.synthetic.main.activity_login.*
+import android.widget.Toast
+import android.databinding.adapters.TextViewBindingAdapter.setText
+import android.util.Log
+
 
 class LoginActivity : AppCompatActivity() {
 
+    private val OAUTH_CLIENT_ID = "ILMvRUaQNzCAYsTvxb59"
+    private val OAUTH_CLIENT_SECRET = "tNuAP0CtD5"
+    private val OAUTH_CLIENT_NAME = "Culin"
+    private lateinit var mOAuthLoginInstance: OAuthLogin
+    private var loginOK: Boolean = false;
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        initData()
     }
 
     override fun onResume() {
         super.onResume()
         var loginButton = this.naverLogin
-        naverLogin.setOnClickListener { view ->
-            var intent = Intent(this, MainActivity::class.java)
-            // TODO : 이런식으로 intent.putExtra(key, value) 식으로 로그인 정보 전달
-            intent.putExtra("userName", "부현식")
-            intent.putExtra("userEmail", "bhs9194@nate.com")
-            startActivity(intent)
-            finish()
+        loginButton.setOAuthLoginHandler(mOAuthLoginHandler)
+        mOAuthLoginInstance.getAccessToken()
+    }
+    fun startCulin(){
+        var intent = Intent(this, MainActivity::class.java)
+        // TODO : 이런식으로 intent.putExtra(key, value) 식으로 로그인 정보 전달
+        intent.putExtra("userName", "부현식")
+        intent.putExtra("userEmail", "bhs9194@nate.com")
+        startActivity(intent)
+        finish()
+    }
+
+    fun initData(){
+        mOAuthLoginInstance = OAuthLogin.getInstance()
+        mOAuthLoginInstance.init(this,OAUTH_CLIENT_ID,OAUTH_CLIENT_SECRET,OAUTH_CLIENT_NAME)
+    }
+
+    private val mOAuthLoginHandler = object : OAuthLoginHandler() {
+        override fun run(success: Boolean) {
+            if (success) {
+                Log.d("로그인", "로그인성공")
+                startCulin()
+            } else {
+                Log.d("로그인", "로그인실패")
+            }
         }
     }
+
+
 }
