8e0f86607b8757ed2c6ce5d7858edfd97e210908
JuRyun <id01096883446@gmail.com>
2018-09-07 13:44:16 +0900

    셀프롤백

--- app/src/main/java/com/caucse/seoulproject/fragment/InfoFragment.kt
+++ app/src/main/java/com/caucse/seoulproject/fragment/InfoFragment.kt
@@ -8,7 +8,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.ImageView
+import android.widget.ScrollView
 import android.widget.TextView
 
 import com.caucse.seoulproject.R
@@ -16,11 +18,13 @@ import com.caucse.seoulproject.data.CultureRow
 import com.caucse.seoulproject.helper.NSearchApiHelper
 import com.caucse.seoulproject.utils.ImageUtil
 import com.caucse.seoulproject.viewmodel.MainViewModel
+import com.google.android.gms.maps.MapView
 import com.nhn.android.maps.NMapContext
 import com.nhn.android.maps.NMapView
 import com.nhn.android.mapviewer.overlay.NMapResourceProvider
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_info.*
 
 
 class InfoFragment : NMapFragment() {
@@ -42,19 +46,23 @@ class InfoFragment : NMapFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         cultureData = mainViewModel.curConcert
-        var view: View= inflater.inflate(R.layout.fragment_info, container, false)
-        var readView:TextView = view.findViewById(R.id.readMoreTextView)
-        var titleView:TextView = view.findViewById(R.id.titleView)
-        var mMapView:NMapView = view.findViewById(R.id.mapView)
-        var titleContent =  cultureData!!.TITLE
-        var infoContent = cultureData!!.CONTENTS
-        var imageView: ImageView = view.findViewById(R.id.infoImageView)
-        var mMapContext:NMapContext = NMapContext(super.getActivity())
+        val view: View= inflater.inflate(R.layout.fragment_info, container, false)
+        //val readView:TextView = view.findViewById(R.id.readMoreTextView)
+        val titleView:TextView = view.findViewById(R.id.titleView)
+        val mMapView:NMapView = view.findViewById(R.id.mapView)
+        val titleContent =  cultureData!!.TITLE
+        val infoContent = cultureData!!.CONTENTS
+        val imageView: ImageView = view.findViewById(R.id.infoImageView)
+        val mMapContext:NMapContext = NMapContext(super.getActivity())
+        val scrollView: ScrollView = view.findViewById(R.id.scrollView2)
+        val mWebView: WebView = view.findViewById(R.id.webView)
+        scrollView.smoothScrollTo(0,0)
 
         mMapContext.onCreate()
 
         titleView.setText(titleContent)
-        readView.setText(infoContent)
+        //readView.setText(infoContent)
+        mWebView.loadData(infoContent,"text/html","UTF-8")
         val url = cultureData!!.MAIN_IMG.toLowerCase()
         ImageUtil.setImage(imageView, url)
 
