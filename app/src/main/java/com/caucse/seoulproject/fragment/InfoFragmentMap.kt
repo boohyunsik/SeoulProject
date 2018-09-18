28e3a6288cd1a7a32149ae659facfe7b249a63b8
Hyunsik Boo <bhs9194@nate.com>
2018-09-19 01:24:05 +0900

    google맵으로 변경

--- app/src/main/java/com/caucse/seoulproject/fragment/InfoFragmentMap.kt
+++ app/src/main/java/com/caucse/seoulproject/fragment/InfoFragmentMap.kt
@@ -2,6 +2,7 @@ package com.caucse.seoulproject.fragment
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,9 @@ import com.caucse.seoulproject.helper.NSearchApiHelper
 import com.caucse.seoulproject.utils.GeoTrans
 import com.caucse.seoulproject.utils.GeoTransPoint
 import com.caucse.seoulproject.viewmodel.MainViewModel
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.nhn.android.maps.NMapContext
 import com.nhn.android.maps.NMapController
 import com.nhn.android.maps.NMapView
@@ -26,23 +30,25 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_info_fragment_map.view.*
 
-class InfoFragmentMap : NMapFragment(), NMapView.OnMapStateChangeListener {
+class InfoFragmentMap : Fragment(), OnMapReadyCallback {
     val TAG = this::class.java.simpleName
 
     private lateinit var nMapView: NMapView
     private var cultureData : CultureRow? = null
     private lateinit var mainViewModel: MainViewModel
+    private var googleMap: GoogleMap? = null
+    private lateinit var mapView: MapView
 
-    private lateinit var nMapViewerResourceProvider: NMapViewerResourceProvider
-    private lateinit var nMapOverlayManager: NMapOverlayManager
-    private lateinit var nMapController: NMapController
-    private lateinit var nMapContext: NMapContext
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel = ViewModelProviders.of(activity!!).get(MainViewModel::class.java)
-        nMapContext = NMapContext(activity)
-        nMapContext.onCreate()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -50,117 +56,60 @@ class InfoFragmentMap : NMapFragment(), NMapView.OnMapStateChangeListener {
         Log.d(TAG, "onCreateView()")
         cultureData = mainViewModel.curConcert
 
-        nMapView = view.naver_mapView
-        nMapView.setClientId(getString(R.string.naver_client_key))
-        nMapContext.setupMapView(nMapView)
-
-        val query = "02-" + cultureData!!.PLACE
-        NSearchApiHelper.getData(context, query)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(
-                        {result ->
-                            Log.d(TAG, "search result = ${result}")
-                            val mapX = result.getString("mapx").toInt()
-                            val mapY = result.getString("mapy").toInt()
-                            moveMapCenter(mapX, mapY)
-                        },
-                        {error ->
-                            Log.d(TAG, "error -> " + error.message + ", 검색어 = $query")},
-                        {Log.d(TAG, "검색어 = ${query}")}
-                )
-
         val locationTextView = view.map_location
         val telTextView = view.map_tel
 
         locationTextView.text = cultureData!!.PLACE
         telTextView.text = cultureData!!.INQUIRY
+        mapView = view.mapView
+        mapView.getMapAsync(this)
         return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        Log.d(TAG, "onMapReady()")
+        val sydney: LatLng = LatLng(37.56, 126.97)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker is Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13f))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
         super.onStart()
-        nMapContext.onStart()
-        Log.d(TAG, "onStart() -> ${nMapView}")
-        nMapView.isClickable = true
-        nMapView.isEnabled = true
-        nMapView.isFocusable = true
-        nMapView.isFocusableInTouchMode = true
-        nMapView.displayZoomControls(true)
-        nMapView.requestFocus()
-
-        nMapView.setBuiltInZoomControls(true, null)
-        nMapView.setOnMapStateChangeListener(this)
-        nMapController = nMapView.mapController
-        nMapViewerResourceProvider = NMapViewerResourceProvider(activity)
-        nMapOverlayManager = NMapOverlayManager(activity, nMapView, nMapViewerResourceProvider)
-    }
-
-    fun moveMapCenter(x: Int, y: Int) {
-        Log.d(TAG, "moveMapCenter($x, $y)")
-        val katec = GeoTransPoint(x.toDouble(), y.toDouble())
-        val oGeo = GeoTrans.convert(GeoTrans.KATEC, GeoTrans.GEO, katec)
-        val lat: Double = oGeo.getY()
-        val lng: Double = oGeo.getX()
-        Log.d(TAG, "lat = ${lat}, long = ${lng}")
-        val currentPoint = NGeoPoint(lat, lng)
-        nMapController.mapCenter = currentPoint
-
-        val poiData = NMapPOIdata(2, nMapViewerResourceProvider)
-        poiData.beginPOIdata(1)
-        poiData.addPOIitem(lng, lat,"example", NMapPOIflagType.PIN, 0)
-        poiData.endPOIdata()
-
-        val poiDataOverlay: NMapPOIdataOverlay = nMapOverlayManager.createPOIdataOverlay(poiData, null)
-        poiDataOverlay.showAllPOIdata(0)
-        poiDataOverlay.onStateChangeListener = IOnStateChangeListener()
-    }
-
-    override fun onAnimationStateChange(p0: NMapView?, p1: Int, p2: Int) {
-        Log.d(TAG, "onAnimationStateChange()")
-    }
-
-    override fun onMapCenterChange(p0: NMapView?, p1: NGeoPoint?) {
-        Log.d(TAG, "onMapCenterChange()")
-    }
-
-    override fun onMapCenterChangeFine(p0: NMapView?) {
-        Log.d(TAG, "onMapCenterChangeFine()")
-    }
-
-    override fun onMapInitHandler(nMapView: NMapView?, nMapError: NMapError?) {
-        if (nMapError == null) {
-            Log.d(TAG, "map init success")
-        } else {
-            Log.e(TAG, "map init error : ${nMapError.message}")
-        }
-    }
-
-    override fun onZoomLevelChange(p0: NMapView?, p1: Int) {
-        Log.d(TAG, "onZoomLevelChange()")
+        mapView.onStart()
     }
 
     override fun onResume() {
         Log.d(TAG, "onResume()")
         super.onResume()
-        nMapContext.onResume()
+        mapView.onResume()
     }
 
     override fun onPause() {
         Log.d(TAG, "onPause()")
         super.onPause()
-        nMapContext.onPause()
+        mapView.onPause()
     }
 
     override fun onStop() {
         Log.d(TAG, "onStop()")
         super.onStop()
-        nMapContext.onStop()
+        mapView.onStop()
     }
 
     override fun onDestroy() {
         Log.d(TAG, "onDestroy()")
-        nMapContext.onDestroy()
         super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
